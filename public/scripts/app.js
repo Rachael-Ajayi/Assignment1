0d732a82f853274cb9edb28fb383cd3054ae7084
Rachael-Ajayi <113180511+Rachael-Ajayi@users.noreply.github.com>
2022-10-02 03:24:45 -0400

    commit 2
    Navigation bar

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+//IIFE - Immediately invoked fuction expression
+
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
